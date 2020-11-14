4d8845d6e6fd6d55de0e9eef7a69d29d3144c73c
Zach Brooks <brooksztb@gmail.com>
2020-11-14 15:51:31 -0700

    Added nuxt/cloudinary and set code highlighter theme
    Added nuxt/cloudinary and set code highlighter theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,79 +1,86 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
-  target: "static",
+  target: 'static',
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: "zbrooksfolio",
+    title: 'zbrooksfolio',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
-  colorMode: {
-    preference: "dark", // default value of $colorMode.preference
-    fallback: "dark", // fallback value if not system preference found
-    hid: "nuxt-color-mode-script",
-    globalName: "__NUXT_COLOR_MODE__",
-    componentName: "ColorScheme",
-    classPrefix: "",
-    classSuffix: "",
-    storageKey: "nuxt-color-mode"
-  },
-
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
-
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
-
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {},
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    "@nuxtjs/tailwindcss",
-    "@nuxtjs/color-mode",
-    "@nuxtjs/fontawesome",
-    "@nuxtjs/svg"
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/fontawesome',
+    '@nuxtjs/svg',
+    '@nuxtjs/cloudinary'
   ],
-
-  tailwindcss: {
-    exposeConfig: true
-  },
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: ["@nuxt/content", "nuxt-webfontloader"],
-  content: {},
+  modules: ['@nuxt/content', 'nuxt-webfontloader'],
+
+  //Module Configuration
+  cloudinary: {
+    cloudName: 'zacharybrooks-dev',
+    useComponent: true
+  },
+  colorMode: {
+    preference: 'dark', // default value of $colorMode.preference
+    fallback: 'dark', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '',
+    storageKey: 'nuxt-color-mode'
+  },
+  content: {
+    markdown: {
+      prism: {
+        theme: 'prism-themes/themes/prism-material-oceanic.css'
+      }
+    }
+  },
   fontawesome: {
     icons: {
       solid: [
-        "faHome",
-        "faEnvelope",
-        "faNewspaper",
-        "faCode",
-        "faUser",
-        "faArrowRight",
-        "faArrowLeft",
-        "faShareAlt",
-        "faThumbsUp",
-        "faTimesCircle"
+        'faEnvelope',
+        'faNewspaper',
+        'faCode',
+        'faUser',
+        'faArrowRight',
+        'faArrowLeft',
+        'faShareAlt',
+        'faThumbsUp',
+        'faTimesCircle'
       ],
-      brands: ["faGithub", "faDev", "faTwitter", "faLinkedin"]
+      brands: ['faGithub', 'faTwitter', 'faLinkedin']
     }
   },
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  tailwindcss: {
+    exposeConfig: true
+  },
   webfontloader: {
     google: {
       families: [
-        "Cabin:300,400,600,700&display=swap",
-        "Open+Sans:300,400&display=swap"
+        'Cabin:300,400,600,700&display=swap',
+        'Open+Sans:300,400&display=swap'
       ]
     },
     //Eventually want to add Uni Neue W05 Regular, Bold, Semibold, Italic from
     monotype: {}
   }
-};
+}
